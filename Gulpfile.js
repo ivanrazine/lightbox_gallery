3be4ac4b6d1e8dd48bb566d9448101fdee8780f9
irazine <ivan.razine@external.engie.com>
2018-03-29 17:17:27 +0200

    init

---
+++ Gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    gulp.src('src/sass/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./css/'))
+});
+
+gulp.task('scripts', function() {
+    gulp.src('src/js/scripts.js')
+        .pipe(gulp.dest('./js/'))
+});
+
+gulp.task('default', function () {
+    gulp.watch('src/sass/**/*.scss',['styles']);
+    gulp.watch('src/js/scripts.js',['scripts']);
+});
+
+
